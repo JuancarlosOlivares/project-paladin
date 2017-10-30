bed1225592b8946a2fe690cb00d8abedbac6fa42
Juan <lublu137@gmail.com>
2017-10-29 20:28:13 -0700

    work in progress - classes

---
+++ something-about-classes/CareTaker.swift
@@ -0,0 +1,14 @@
+//
+//  PhoneUnit.swift
+//  Project Paladin
+//
+//  Created by Juancarlos Olivares on 10/24/17.
+//  Copyright © 2017 Olive Tree Ent. All rights reserved.
+//
+
+import Foundation
+
+class CareTaker: AppDelegate {
+    //stuff goes here
+    
+}
---
+++ something-about-classes/Vu.swift
@@ -0,0 +1,13 @@
+//
+//  Vu.swift
+//  Project Paladin
+//
+//  Created by Juancarlos Olivares on 10/24/17.
+//  Copyright © 2017 Olive Tree Ent. All rights reserved.
+//
+
+import Foundation
+
+class Vu: Sensor{
+    
+}
